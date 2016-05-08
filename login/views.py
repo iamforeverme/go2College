4588e99c18e9128aa49f03c8a378a457b06d318f
kevin <iamforeverme@163.com>
2016-05-08 21:18:03 +0800

    Find out ways to get session status

--- login/views.py
+++ login/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return HttpResponse("Hello, world. You're at the login index.")
 
 
+@login_required
 def profile(request):
+    #if request.user.is_authenticated():
+        # Do something for authenticated users.
     return HttpResponse("Hello, world. Here is your profile.")
